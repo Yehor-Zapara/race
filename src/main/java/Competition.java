f6bd5381a7a702efdaa2fec45a7ffa59035e86de
Yehor Zapara <egor.zapara1994@gmail.com>
2021-11-25 11:55:19 +0200

    Redundant imports were deleted

--- src/main/java/Competition.java
+++ src/main/java/Competition.java
@@ -6,14 +6,10 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
